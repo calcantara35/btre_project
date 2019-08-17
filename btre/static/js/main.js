205d31604b5706c8ca90420e421158fe9f126a19
Cesar Alcantara <cja5391@psu.edu>
2019-08-17 16:41:58 -0400

    message alerts, added some jquery for fade

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,2 +1,8 @@
 const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
+document.querySelector(".year").innerHTML = date.getFullYear();
+
+// Time out for error message
+setTimeout(function() {
+  // jQuery
+  $("#message").fadeOut("slow");
+}, 3000);
